5dad735557af8399d6b8b5311324b2eb7093ecd0
KayAhn0126 <dkseotn123@gmail.com>
2022-11-23 18:56:57 +0900

    refactor: Table view 추가 및 할 일 등록 버튼 구현

---
+++ ToDoList/ToDoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by Kay on 2022/11/23.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var dont: Bool
+}
--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
 
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default) { [unowned self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, dont: false)
+            self.tasks.append(task)
+            self.tableView.reloadData()
+        }
+        
+        let cancelButton = UIAlertAction(title: "취소", style: .destructive)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+}
